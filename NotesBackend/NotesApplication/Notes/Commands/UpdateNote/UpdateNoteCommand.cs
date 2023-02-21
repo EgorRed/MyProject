9f02a359265218d174a03d3edf745020af2c93a2
Egor Red <egor.red02@gmail.com>
2023-02-21 16:00:41 +0300

    integration MediatR in project

---
+++ NotesBackend/NotesApplication/Notes/Commands/UpdateNote/UpdateNoteCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Threading.Tasks;
+
+namespace NotesApplication.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommand : IRequest<Unit>
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+    }
+}
---
+++ NotesBackend/NotesApplication/Notes/Commands/UpdateNote/UpdateNoteCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using NotesApplication.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using NotesDomain;
+
+
+namespace NotesApplication.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandHandler
+        : IRequestHandler<UpdateNoteCommand, Unit>
+    {
+        private readonly INotesDbContext _dbContext;
+
+        public UpdateNoteCommandHandler(INotesDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Unit> Handle(UpdateNoteCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbContext.Notes.FirstOrDefaultAsync(note =>
+            note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            entity.Details = request.Details;
+            entity.Title = request.Title;
+            entity.EditDate = DateTime.Now;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+
+
+    }
+}
