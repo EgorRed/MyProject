9c2c3e7379a2af02d8a68e22558ee19c35daec93
Egor Red <egor.red02@gmail.com>
2023-02-21 19:57:28 +0300

    Add Get note list query

---
+++ NotesBackend/NotesApplication/Notes/Queries/GetNoteList/NoteListVm.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace NotesApplication.Notes.Queries.GetNoteList
+{
+    public class NoteListVm
+    {
+        public IList<NoteLockupDto> Notes { get; set; }
+    }
+}
---
+++ NotesBackend/NotesApplication/Notes/Queries/GetNoteList/NoteLockupDto.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using NotesApplication.Common.Mapping;
+using NotesDomain;
+using System;
+
+
+namespace NotesApplication.Notes.Queries.GetNoteList
+{
+    public class NoteLockupDto : IMapWith<Note>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteLockupDto>()
+                .ForMember(noteDto => noteDto.Id,
+                opt => opt.MapFrom(note => note.Id))
+                .ForMember(noteDto => noteDto.Title,
+                opt => opt.MapFrom(note => note.Title));
+        }
+    }
+}
