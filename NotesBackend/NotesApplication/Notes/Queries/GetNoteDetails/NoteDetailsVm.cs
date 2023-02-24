7176efae9b54ecbd8cbdafb829212d007b97da42
Egor Red <egor.red02@gmail.com>
2023-02-24 11:08:56 +0300

    cleaning the file from garbage

--- NotesBackend/NotesApplication/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
+++ NotesBackend/NotesApplication/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
@@ -2,10 +2,6 @@
 using NotesApplication.Common.Mapping;
 using NotesDomain;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NotesApplication.Notes.Queries.GetNoteDetails
 {
