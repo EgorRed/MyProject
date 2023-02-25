98f439d5956a090eed6ed477dbd09290c1c8ed94
Egor Red <egor.red02@gmail.com>
2023-02-25 09:59:36 +0300

    Add DependencyInjection

---
+++ NotesBackend/NotesPersistems/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using NotesApplication.Interfaces;
+
+namespace NotesPersistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistens(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<INotesDbContext>(proviter => proviter.GetService<NotesDbContext>());
+            return services;
+        }
+    }
+}
