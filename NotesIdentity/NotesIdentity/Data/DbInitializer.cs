a3883c70976101e693d247f297a6635d11c7c64a
Egor Red <egor.red02@gmail.com>
2023-02-28 15:52:32 +0300

    Add database in authorization service

---
+++ NotesIdentity/NotesIdentity/Data/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace NotesIdentity.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(AuthDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
--- NotesIdentity/NotesIdentity/Program.cs
+++ NotesIdentity/NotesIdentity/Program.cs
@@ -1,16 +1,62 @@
+using System;
 using NotesIdentity;
-
-
+using Microsoft.Extensions.Configuration;
+using NotesIdentity.Data;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.EntityFrameworkCore;
+using NotesIdentity.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetValue<string>("DbConnection");
+
+
+builder.Services.AddDbContext<AuthDbContext>(option =>
+{
+    option.UseSqlite(connectionString);
+});
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 8;
+    config.Password.RequireDigit = false;
+    config.Password.RequireNonAlphanumeric = false;
+    config.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
     .AddInMemoryApiResources(Configuration.ApiResources)
     .AddInMemoryIdentityResources(Configuration.IdentityResources)
     .AddInMemoryApiScopes(Configuration.ApiScopes)
     .AddInMemoryClients(Configuration.Clients)
     .AddDeveloperSigningCredential();
 
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "Notes.Identity.Cookie";
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Logout";
+});
+
 var app = builder.Build();
+
+using (IServiceScope serviceProvider = builder.Services.BuildServiceProvider().CreateScope())
+{
+    try
+    {
+        var context = serviceProvider.ServiceProvider.GetRequiredService<AuthDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        //TODO: create exception
+    }
+}
+
 app.UseIdentityServer();
 app.Run();
