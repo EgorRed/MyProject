947066e6cab4e426c08adec2d96eb8f13e8c3f0b
Egor Red <egor.red02@gmail.com>
2023-02-28 13:15:48 +0300

    Add configuration

---
+++ NotesIdentity/NotesIdentity/Configuration.cs
@@ -0,0 +1,65 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Security;
+
+namespace NotesIdentity
+{
+    public class Configuration
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("NotesWebAPI", "Web API")
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static List<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("NoteWebAPI", "Web API", new[] {JwtClaimTypes.Name} )
+                {
+                    Scopes = { "NoteWebAPI" }
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "notes-web-api",
+                    ClientName = "Notes Web",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RequirePkce = true,
+                    RedirectUris =
+                    {
+                        "http://.../signin-oidc"
+                    },
+                    AllowedCorsOrigins =
+                    {
+                        "http://..."
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "http:/.../signout-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "NotesWebAPI"
+                    },
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
+
+    }
+}
--- NotesIdentity/NotesIdentity/Program.cs
+++ NotesIdentity/NotesIdentity/Program.cs
@@ -1,6 +1,16 @@
+using NotesIdentity;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryApiResources(Configuration.ApiResources)
+    .AddInMemoryIdentityResources(Configuration.IdentityResources)
+    .AddInMemoryApiScopes(Configuration.ApiScopes)
+    .AddInMemoryClients(Configuration.Clients)
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseIdentityServer();
 app.Run();
